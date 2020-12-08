9500801fd9684568550b89c880e6bc7775bd9f42
christopherj525 <christopher.hurless@gmail.com>
2020-12-08 09:46:52 +0300

    continuing with consolidation into run level files.

--- CIStoPLIST/system.py
+++ CIStoPLIST/system.py
@@ -1,10 +1,9 @@
 import subprocess
-import os
+import re
 from CIStoPLIST import shared
 
 
-# Beginning of the "System Preferences" block, relating to CIS 02
-# Additional controls are found in system_elevated.py and user.py
+
 def bluetooth():
     """
     :return: a dictionary of settings for writing to a plist file.
@@ -87,11 +86,8 @@ def efi():
     else:
         efi_plist.update({'IntegrityCheck': 'Check EFI integrity'})
     return efi_plist
-# End of System Preferences block, relating to  CIS 02
 
 
-# Beginning of the "Network Configurations" Block, relating to CIS 04
-# Additional controls are found in system_elevated.py and user.py
 def network_configuration():
     # 4.1 Disable Bonjour advertising service
     # the key "NoMulticastAdvertisements" does not exist by default.
@@ -125,6 +121,121 @@ def network_configuration():
     return network_configurations_plist
 
 
+def password_policy():
+    password_policy_plist = {}
+
+    # 5.2.1 Configure account lockout threshold (Scored)
+    failed_logins = subprocess.Popen("pwpolicy -getaccountpolicies | "
+                                     "grep -A 1 'policyAttributeMaximumFailedAuthentications' | "
+                                     "tail -1 | cut -d'>' -f2 | cut -d '<' -f1",
+                                     stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    failed_logins_out = failed_logins.communicate()
+    password_policy_plist.update({'MaxFailedLoginAttempts': int(failed_logins_out[0].decode('utf-8'))})
+
+    # 5.2.2 Set a minimum password length (Scored)
+    min_pass_lenth = subprocess.Popen('pwpolicy -getaccountpolicies | egrep "15 characters"',
+                                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    min_pass_lenth_out = min_pass_lenth.communicate()
+    if "15 characters" in min_pass_lenth_out[0].decode('utf-8'):
+        password_policy_plist.update({'MinimumPasswordLength': 15})
+    else:
+        password_policy_plist.update({'MinimumPasswordLength': 0})
+
+    # 5.2.3 Complex passwords must contain an Alphabetic Character (Not Scored)
+    pass_contains_alpha = subprocess.Popen('pwpolicy -getaccountpolicies | egrep "Alpha"',
+                                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    pass_contains_alpha_out = pass_contains_alpha.communicate()
+    if "com.apple.policy.legacy.requiresAlpha" in pass_contains_alpha_out[0].decode('utf-8'):
+        password_policy_plist.update({'PasswordRequiresAlpha': 1})
+    else:
+        password_policy_plist.update({'PasswordRequiresAlpha': 0})
+
+    # 5.2.4 Complex passwords must contain a Numeric Character (Not Scored)
+    pass_contains_numeric = subprocess.Popen('pwpolicy -getaccountpolicies | egrep "Numeric"',
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    pass_contains_numeric_out = pass_contains_numeric.communicate()
+    if "com.apple.policy.legacy.requiresNumeric" in pass_contains_numeric_out[0].decode('utf-8'):
+        password_policy_plist.update({'PasswordRequiresNumeric': 1})
+    else:
+        password_policy_plist.update({'PasswordRequiresNumeric': 0})
+
+    # 5.2.5 Complex passwords must contain a Special Character (Not Scored)
+    pass_contains_special = subprocess.Popen('pwpolicy -getaccountpolicies | egrep "1 special"',
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    pass_contains_special_out = pass_contains_special.communicate()
+    if "Password must have at least 1 special character" in pass_contains_special_out[0].decode('utf-8'):
+        password_policy_plist.update({'PasswordRequiresSpecial': 1})
+    else:
+        password_policy_plist.update({'PasswordRequiresSpecial': 0})
+
+    # 5.2.6 Complex passwords must uppercase and lowercase letters (Not Scored)
+    pass_contains_upper_lower = subprocess.Popen('pwpolicy -getaccountpolicies | '
+                                                 'egrep "com.apple.uppercaseAndLowercase"',
+                                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    pass_contains_upper_lower_out = pass_contains_upper_lower.communicate()
+    if "com.apple.uppercaseAndLowercase" in pass_contains_upper_lower_out[0].decode('utf-8'):
+        password_policy_plist.update({'PasswordRequiresUpperAndLower': 1})
+    else:
+        password_policy_plist.update({'PasswordRequiresUpperAndLower': 0})
+
+    # 5.2.7 Password Age (Scored)
+    pass_age = subprocess.Popen('pwpolicy -getaccountpolicies | egrep policyAttributeExpiresEveryNDays',
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    pass_age_out = pass_age.communicate()
+
+    if "policyAttributeExpiresEveryNDays" in pass_age_out[0].decode('utf-8'):
+        password_age = re.findall(r'[0-9]+', pass_age_out[0].decode('utf-8'))
+        password_policy_plist.update({'PasswordAge': pass_age})
+    else:
+        password_policy_plist.update({'PasswordAge': 0})
+
+    # 5.2.8 Password History (Scored)
+    differ_from_past = subprocess.Popen('pwpolicy -getaccountpolicies | egrep "differ from past"',
+                                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    differ_from_past_out = differ_from_past.communicate()
+    if "Password must differ from past 15 passwords" in differ_from_past_out[0].decode('utf-8'):
+        password_policy_plist.update({'DifferFromPast': 15})
+    else:
+        password_policy_plist.update({'DifferFromPast': 0})
+    return password_policy_plist
+
+
+def system_integrity():
+    # 5.23 System Integrity Protection status (Scored)
+    system_integrity_plist = {}
+    system_integrity_status = subprocess.run(['/usr/bin/csrutil', 'status'],
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    system_integrity_plist.update(
+        {'SystemIntegrityStatus': system_integrity_status.stdout.decode('utf-8').rstrip("\n")[36:-1]})
+    return system_integrity_plist
+
+
+def user_account_status():
+    # needs to be tested.
+    # 5.11 Do not enable the "root" account (Scored)
+    users_accounts_plist = {}
+
+    root_disabled = subprocess.Popen('dscl . -read /Users/root AuthenticationAuthority',
+                                     stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    root_disabled_out = root_disabled.communicate()
+    users_accounts_plist.update({'RootDisabled': root_disabled_out[0].decode('utf-8').rstrip("\n")})
+
+    # 5.16 Disable ability to login to another user's active and locked session (Scored)
+    disable_login_other_user = subprocess.Popen('/usr/bin/security authorizationdb read system.login.screensaver '
+                                                '2>/dev/null | /usr/bin/grep -A 1 "<array>" | /usr/bin/awk -F "<|>" '
+                                                '"END{ print $3 }"', stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                                shell=True)
+    disable_login_other_user_out = disable_login_other_user.communicate()
+    users_accounts_plist.update(
+        {'DisableLoginUserActiveSession': disable_login_other_user_out[0].decode('utf-8')[10:-10]})
+
+    # 5.20 Disable Fast User Switching (Not Scored)
+
+    fast_user_switching = subprocess.run(['defaults', 'read', '/Library/Preferences/.GlobalPreferences',
+                                          'MultipleSessionEnabled'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    users_accounts_plist.update({'FastUserSwitching': bool(fast_user_switching.stdout.decode('utf-8'))})
+    return users_accounts_plist
+
 
 if __name__ == '__main__':
     system_plist = {}
@@ -134,6 +245,9 @@ if __name__ == '__main__':
     # system_plist.update(java())
     # system_plist.update(efi())
     system_plist.update(network_configuration())
+    system_plist.update(password_policy())
+    system_plist.update(system_integrity())
+    system_plist.update(user_account_status())
     print(system_plist)
     shared.plist_create('/tmp/system_cis.plist', system_plist)
 
--- CIStoPLIST/user.py
+++ CIStoPLIST/user.py
@@ -2,6 +2,7 @@ import subprocess
 from pathlib import Path
 from CIStoPLIST import shared
 
+
 def screen_saver():
     # 2.3 Desktop & Screen Saver
     # 2.3.1 Set an inactivity interval of 20 minutes or less for the screen saver (Scored)
@@ -96,11 +97,32 @@ def time_machine():
         print("This may not work on this version of the OS")
     return time_machine_plist
 
+def keychain():
+    # 5.7 Automatically lock the login keychain for inactivity (Scored)
+    keychain_plist = {}
+    keychain_timeout = subprocess.run(['security', 'show-keychain-info'],
+                                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    keychain_plist.update({'KeychainTimeout': keychain_timeout.stdout.decode('utf-8').rstrip("\n")})
 
+    # 5.8 Ensure login keychain is locked when the computer sleeps (Scored)
+    keychain_sleep_lock = subprocess.run(['security', 'show-keychain-info'],
+                                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    keychain_plist.update({'KeychainSleepLock': keychain_sleep_lock.stdout.decode('utf-8').rstrip("\n")})
+
+    # 5.9 Enable OCSP and CRL certificate checking (Scored)
+    certificate_check_CRL = subprocess.run(['defaults', 'read', 'com.apple.security.revocation', 'CRLStyle'],
+                                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    keychain_plist.update({'TrustCRL': certificate_check_CRL.stdout.decode('utf-8').rstrip("\n")})
+
+    certificate_check_OCSP = subprocess.run(['defaults', 'read', 'com.apple.security.revocation', 'OCSPStyle'],
+                                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    keychain_plist.update({'TrustOCSP': certificate_check_OCSP.stdout.decode('utf-8').rstrip("\n")})
+    return  keychain_plist
 
 
 if __name__ == '__main__':
     print(screen_saver())
     print(icloud())
     print(time_machine())
+    print(keychain())
 
